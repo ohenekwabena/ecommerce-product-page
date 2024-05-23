85e39fd764f6bbff4800aa878bcc883ba5177e32
ohenekwabena <adjeiasarejesse@gmail.com>
2024-05-23 00:03:18 +0000

    Edit mobile nav animation

--- src/components/MobileLinks/MobileLinks.js
+++ src/components/MobileLinks/MobileLinks.js
@@ -5,18 +5,20 @@ import { NavLinks } from "../../constants";
 import { motion } from "framer-motion";
 import styles from "./MobileLinks.module.css";
 import UnstyledButton from "../UnstyledButton";
+import closeButton from "../../../public/images/icon-close.svg";
+import Image from "next/image";
 import Link from "next/link";
 
 function MobileLinks({ show, setShow }) {
   return (
     <Modal open={show} close={() => setShow(false)}>
-      <motion.div className={styles.wrapper} initial={{ x: "-100%" }} animate={{ x: 0 }}>
+      <motion.div className={styles.wrapper} initial={{ x: "-100%" }} animate={{ x: 0 }} transition={{ duration: 0.5 }}>
         <UnstyledButton
           onClick={() => {
             setShow(false);
           }}
         >
-          <img src="/images/icon-close.svg" alt="close" />
+          <Image src={closeButton} alt="close" width={20} height={20} />
         </UnstyledButton>
         <ul className={styles.links}>
           {NavLinks.map((link) => (
